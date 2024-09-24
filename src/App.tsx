dbd0eeee31be68ff2888f1b04b686289148fdd8c
fbarroso <fbarroso@cuc.edu.co>
2024-09-24 13:12:04 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import milogo from "../src/assets/cristiano_ronaldo_avatar_icon_263214.png"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <img src={milogo} alt="logo react" />
+      <h1>hola mundo</h1>
+      <p>proyecto react</p>
+    </div>
+    </>
+  )
+}
+
+export default App
